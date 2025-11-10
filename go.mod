8a594735565137f5d968aa38e6a95a1c0a48b5fb
Krzysztof Kuś (Ubuntu) <kuskrz@gmail.com>
2025-11-10 13:38:58 +0100

    processing TAG

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module kus/krzysztof/titler
 
 go 1.25.3
+
+require golang.org/x/net v0.46.0 // indirect
--- handlers/gettag.go
+++ handlers/gettag.go
@@ -2,9 +2,15 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
+	"io"
+	"kus/krzysztof/titler/environment"
 	"kus/krzysztof/titler/logging"
 	"kus/krzysztof/titler/requests"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 func GetTag(w http.ResponseWriter, r *http.Request) {
@@ -20,10 +26,39 @@ func GetTag(w http.ResponseWriter, r *http.Request) {
 	logging.Logf(logging.DEBUG, "Request from: %s %s %s\n", r.RemoteAddr, r.Method, r.URL.Path)
 	logging.Log(logging.DEBUG, input.Url)
 
-	res, err := http.Get(input.Url)
-	if err == nil {
-		res.Body.Close()
-	} else {
+	resp, err := http.Get(input.Url)
+	if err != nil {
 		logging.Log(logging.ERROR, err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logging.Log(logging.ERROR, err.Error())
+		return
+	}
+
+	tokenizer := html.NewTokenizer(strings.NewReader(string(body)))
+	var isRightToken bool
+	for {
+		tokenType := tokenizer.Next()
+		switch tokenType {
+		case html.ErrorToken:
+			logging.Log(logging.ERROR, "cannot parse HTML token")
+			return
+		case html.StartTagToken:
+			token := tokenizer.Token()
+			isRightToken = token.Data == environment.EnvVars["TAG"]
+		case html.TextToken:
+			if isRightToken {
+				token := tokenizer.Token()
+				fmt.Println(token)
+				return
+			}
+			isRightToken = false
+		default:
+			isRightToken = false
+		}
 	}
 }
